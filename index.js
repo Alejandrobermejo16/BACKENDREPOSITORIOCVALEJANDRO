d34572a1d0f359a828bc0408aacc7ceab62f2515
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2024-04-15 15:28:23 +0200

    prueba2

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const express = require("express");
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const port = process.env.PORT || 3000;
+// Middleware para analizar el cuerpo de la solicitud JSON
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
+// Configurar el transporter para enviar correos
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'alejandrobermejomendez170712@gmail.com',
+    pass: 'hkbj tofw gaoe xqpp' 
+  }
 });
 
-app.listen(port, () => {
-  console.log(`port runing in http://localhost:${port}`);
+// Ruta para enviar correos
+app.post('/', (req, res) => {
+  const { destinatario, asunto, mensaje } = req.body;
+
+  // Configurar el contenido del correo
+  const mailOptions = {
+    from: 'alejandrobermejomendez170712@gmail.com',
+    to: destinatario,
+    subject: asunto,
+    text: mensaje
+  };
+
+  // Enviar el correo
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error al enviar el correo');
+    } else {
+      console.log('Correo enviado: ' + info.response);
+      res.status(200).send('Correo enviado correctamente');
+    }
+  });
+});
+
+// Ruta de inicio
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo desde el backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
