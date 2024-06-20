5ab9b651c3213b7d8d85d91491be325a71144f9c
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-20 15:47:00 +0200

    pru

--- user.js
+++ user.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ||  3001;
 
 app.use(bodyParser.json());
 
