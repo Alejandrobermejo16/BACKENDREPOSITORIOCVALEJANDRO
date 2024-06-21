762b61f95d5454dfcfb8365101df77f2faf2b938
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-21 13:33:17 +0200

    prueba

--- users.js
+++ users.js
@@ -6,13 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware para analizar el cuerpo de la solicitud JSON
 app.use(bodyParser.json());
 
+// Configurar MongoDB
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Middleware para conectar a MongoDB antes de cada solicitud
 const connectMongoDB = async () => {
-  if (!client.topology || !client.topology.isConnected()) {
+  if (!client.isConnected()) {
     try {
       await client.connect();
       console.log('Conexión establecida correctamente con MongoDB');
@@ -23,15 +26,16 @@ const connectMongoDB = async () => {
   }
 };
 
+// Middleware asincrónico para conectar MongoDB antes de cada solicitud
 app.use(async (req, res, next) => {
   await connectMongoDB();
   req.dbClient = client;
   next();
 });
 
-app.post('/api/users', async (req, res, next) => {
+// Ruta para crear usuarios
+app.post('/api/users', async (req, res) => {
   const { name, email, password } = req.body;
-  console.log('Datos recibidos:', { name, email, password });
   const dbClient = req.dbClient;
 
   try {
@@ -40,27 +44,27 @@ app.post('/api/users', async (req, res, next) => {
 
     const existingUser = await collection.findOne({ $or: [{ name }, { email }] });
     if (existingUser) {
-      console.log('Usuario ya existe:', existingUser);
       return res.status(400).json({ message: 'User already exists' });
     }
 
     const newUser = { name, email, password };
     const result = await collection.insertOne(newUser);
-    console.log('Usuario creado:', result.insertedId);
     res.status(201).json({ message: 'User created successfully', userId: result.insertedId });
   } catch (error) {
     console.error('Error al crear usuario:', error);
-    next(error); 
+    res.status(500).json({ message: 'Error creating user' });
   }
 });
 
+// Middleware de manejo de errores
 app.use((err, req, res, next) => {
   console.error('Error in Express middleware:', err);
   res.status(500).json({ message: 'Something broke!' });
 });
 
+// Escuchar en el puerto
 app.listen(PORT, () => {
-  console.log(`API de usuarios corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
 module.exports = app;
