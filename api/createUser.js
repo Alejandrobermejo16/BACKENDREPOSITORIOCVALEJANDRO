cdaec26c6a6ed216426f6b17e726f5b1b87ec8e7
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-24 16:01:10 +0200

    prueba

--- api/createUser.js
+++ api/createUser.js
@@ -1,28 +1,32 @@
 const express = require('express');
+const app = express()
 const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-const uri = process.env.MONGODB_URI; // Debes configurar esto en tu archivo .env
+app.use(
+  cors({
 
-// Configuración del cliente MongoDB
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+    origin: "https://abmprojects-7kay.vercel.app",
+  })
+
+  )
+
+
+const router = express.Router();
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
 // Middleware para permitir solicitudes CORS desde cualquier origen (solo para pruebas locales)
-app.use(cors());
+router.use(cors());
 
 // Middleware para analizar el cuerpo de la solicitud JSON
-app.use(bodyParser.json());
+router.use(bodyParser.json());
 
 // Middleware para conectar a MongoDB antes de cada solicitud
 const connectMongoDB = async () => {
-  if (!client.isConnected()) {
+  if (!client.topology || !client.topology.isConnected()) {
     try {
       await client.connect();
       console.log('Conexión establecida correctamente con MongoDB');
@@ -34,47 +38,39 @@ const connectMongoDB = async () => {
 };
 
 // Middleware asincrónico para conectar MongoDB antes de cada solicitud
-app.use(async (req, res, next) => {
+router.use(async (req, res, next) => {
   await connectMongoDB();
   req.dbClient = client;
   next();
 });
 
 // Ruta para crear usuarios
-app.post('/api/createUser', async (req, res) => {
-  const { name, email, password } = req.body;
+router.post('/api/users/create', async (req, res) => {
+  const { name, email } = req.body;
   const dbClient = req.dbClient;
 
   try {
     const database = dbClient.db('abmUsers');
     const collection = database.collection('users');
 
-    const existingUser = await collection.findOne({ email });
+    const existingUser = await collection.findOne({ $or: [{ name }, { email }] });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const newUser = { name, email, password };
+    const newUser = { name, email };
     const result = await collection.insertOne(newUser);
     res.status(201).json({ message: 'User created successfully', userId: result.insertedId });
   } catch (error) {
-    console.error('Error creating user:', error);
+    console.error('Error al crear usuario:', error);
     res.status(500).json({ message: 'Error creating user' });
   }
 });
 
-// Ruta de prueba para verificar el funcionamiento del servidor
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo desde el backend!');
-});
-
 // Middleware de manejo de errores
-app.use((err, req, res, next) => {
+router.use((err, req, res, next) => {
   console.error('Error in Express middleware:', err);
   res.status(500).json({ message: 'Something broke!' });
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+module.exports = router;
