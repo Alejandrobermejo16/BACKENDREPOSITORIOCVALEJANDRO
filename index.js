48f5cbc96522ffd8b9376bc436c739d7ac869ffe
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-24 14:30:31 +0200

    pruebas

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Habilitar CORS globalmente para todas las rutas
+// Middleware para permitir solicitudes CORS desde un origen específico
 app.use(cors({
-  origin: '*', // Permitir solicitudes desde cualquier origen (ajustar según necesidades)
+  origin: 'https://abmprojects-7kay.vercel.app', // Cambia esto por tu dominio frontend
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Custom-Header'], // Encabezados permitidos (incluyendo X-Custom-Header)
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
   credentials: true // Permite enviar credenciales (cookies)
 }));
-
 // Middleware para analizar el cuerpo de la solicitud JSON
 app.use(bodyParser.json());
-
+// Configurar el transporter para enviar correos
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'alejandrobermejomendez170712@gmail.com',
+    pass: 'hkbj tofw gaoe xqpp'
+  }
+});
+// Ruta para enviar correos
+app.post('/', (req, res) => {
+  const { destinatario, asunto, mensaje } = req.body;
+  // Configurar el contenido del correo
+  const mailOptions = {
+    from: 'alejandrobermejomendez170712@gmail.com',
+    to: destinatario,
+    subject: asunto,
+    text: mensaje
+  };
+  // Enviar el correo
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error al enviar el correo');
+    } else {
+      console.log('Correo enviado: ' + info.response);
+      res.status(200).send('Correo enviado correctamente');
+    }
+  });
+});
 // Ruta de inicio
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo desde el backend!');
 });
-
-// Ruta de inicio
-app.get('/api/users', (req, res) => {
-  res.send('creacion de usuarios');
-});
-
-// Rutas de la API
-const usersRouter = require('./api/users');
-const createUserRouter = require('./api/createUser');
-const getUserByIdRouter = require('./api/getUserById');
-
-app.use('/users', usersRouter); // Ruta para otras operaciones relacionadas con usuarios
-app.use('/api/users/create', createUserRouter);
-
-// Ruta para obtener un usuario por ID
-app.use('/api/users/:userId', getUserByIdRouter);
-
+// Usar el router de usuarios
+app.use('/api/users', usersRouter);
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
