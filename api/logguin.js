7c40d6c09d6cab57f672567ca8ad2c7a388dea6c
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-26 15:12:48 +0200

    nueva prueba

--- api/logguin.js
+++ api/logguin.js
@@ -1,15 +1,26 @@
+
+
+
+
+// EN ESTE ARCHIVO SE HACE BIEN EL POST Y EL GET
+
+
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+
 const router = express.Router();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
 // Middleware para permitir solicitudes CORS desde cualquier origen (solo para pruebas locales)
 router.use(cors());
+
 // Middleware para analizar el cuerpo de la solicitud JSON
 router.use(bodyParser.json());
+
 // Middleware para conectar a MongoDB antes de cada solicitud
 const connectMongoDB = async () => {
   try {
@@ -22,6 +33,7 @@ const connectMongoDB = async () => {
     throw error;
   }
 };
+
 // Middleware asincrónico para conectar MongoDB antes de cada solicitud
 router.use(async (req, res, next) => {
   try {
@@ -36,33 +48,34 @@ router.use(async (req, res, next) => {
   }
 });
 
-// Ruta para verificar existencia de usuario y autenticación
-router.post('/loggin', async (req, res) => {
+// Ruta obtener usuarios
+router.post('/logguin', async (req, res) => {
   const { email, password } = req.body;
   const dbClient = req.dbClient;
+
 
   try {
     const database = dbClient.db('abmUsers');
     const collection = database.collection('users');
-    // Buscar usuario por email
-    const existingUser = await collection.findOne({ email, password });
-    if (!existingUser) {
-      return res.status(404).json({ message: 'User not found' });
+
+    const existingUser = await collection.findOne({ $or: [{ email }, { password }] });
+    if (existingUser) {
+      return res.status(200).json({ message: 'User already exists' });
     }
-    // Verificar contraseña
-    if (existingUser.password !== password) {
-      return res.status(401).json({ message: 'Incorrect password' });
-    }
-    // Usuario autenticado correctamente
-    res.status(200).json({ message: 'User authenticated successfully', userId: existingUser._id });
+
+    const newUser = { email, password };
+    const result = await collection.insertOne(newUser);
+    res.status(400).json({ message: 'User not found', userId: result.insertedId });
   } catch (error) {
-    console.error('Error al autenticar usuario:', error);
-    res.status(500).json({ message: 'Error authenticating user' });
+    console.error('Error al crear usuario:', error);
+    res.status(500).json({ message: 'Error creating user' });
   }
 });
+
 // Middleware de manejo de errores
 router.use((err, req, res, next) => {
   console.error('Error in Express middleware:', err);
   res.status(500).json({ message: 'Something broke!' });
 });
+
 module.exports = router;
