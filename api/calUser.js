a56ef3b1841a628b09ada54ea73694fc3c49007b
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-19 09:34:17 +0200

    pruebas

--- api/calUser.js
+++ api/calUser.js
@@ -7,13 +7,9 @@ const router = express.Router();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-// Middleware para permitir solicitudes CORS desde cualquier origen (solo para pruebas locales)
 router.use(cors());
-
-// Middleware para analizar el cuerpo de la solicitud JSON
 router.use(bodyParser.json());
 
-// Middleware asincrónico para conectar MongoDB antes de cada solicitud
 router.use(async (req, res, next) => {
   try {
     if (!client.topology || !client.topology.isConnected()) {
@@ -28,7 +24,6 @@ router.use(async (req, res, next) => {
   }
 });
 
-// Ruta para recibir y guardar las calorías
 router.post('/cal', async (req, res) => {
   const { userEmail, calories } = req.body;
 
@@ -40,10 +35,10 @@ router.post('/cal', async (req, res) => {
     const db = req.dbClient.db('abmUsers');
     const collection = db.collection('users');
 
-    // Actualiza el documento del usuario con las calorías
     const result = await collection.updateOne(
-      { $push: { calories: { value: calories, date: new Date() } } },
-      { upsert: true } // Crea un nuevo documento si no existe
+      { userEmail }, // Filtro para encontrar el documento
+      { $push: { calories: { value: calories, date: new Date() } } }, // Actualización
+      { upsert: true } // Crear un nuevo documento si no existe
     );
 
     res.status(201).json({ message: 'Calories added successfully', data: result });
@@ -53,7 +48,6 @@ router.post('/cal', async (req, res) => {
   }
 });
 
-// Middleware de manejo de errores
 router.use((err, req, res, next) => {
   console.error('Error in Express middleware:', err);
   res.status(500).json({ message: 'Something broke!' });
