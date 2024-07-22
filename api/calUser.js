256d58791882bc8d837f50b7373da665b45793e8
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-22 16:51:32 +0200

    pruebas

--- api/calUser.js
+++ api/calUser.js
@@ -11,6 +11,7 @@ const client = new MongoClient(uri);
 router.use(cors());
 router.use(bodyParser.json());
 
+// Middleware para conectar a la base de datos
 router.use(async (req, res, next) => {
   try {
     if (!client.topology || !client.topology.isConnected()) {
@@ -109,10 +110,14 @@ router.post('/cal', async (req, res) => {
   }
 });
 
-// Cron job para restablecer las calorías a 0
-cron.schedule('49 16 * * *', async () => {
-  console.log('Cron job ejecutándose para restablecer calorías a 0...');
+// Cron job para restablecer las calorías a 0 cada minuto
+cron.schedule('* * * * *', async () => {
+  console.log('Cron job ejecutándose cada minuto para restablecer calorías a 0...');
   try {
+    if (!client.topology || !client.topology.isConnected()) {
+      await client.connect();
+      console.log('Conexión establecida correctamente con MongoDB desde cron job');
+    }
     const db = client.db('abmUsers');
     const collection = db.collection('users');
 
