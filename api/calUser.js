cb9dc4ddebd6f2d9985d2797bbb79c34359b5ddb
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-22 14:07:09 +0200

    reset de calorias a las 0000

--- api/calUser.js
+++ api/calUser.js
@@ -122,7 +122,7 @@ app.post('/api/resetCalories', async (req, res) => {
 });
 
 // Configuración del cron job para restablecer calorías a 00:00
-cron.schedule('0 13 * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
   try {
     await client.connect();
     const db = client.db('abmUsers');
