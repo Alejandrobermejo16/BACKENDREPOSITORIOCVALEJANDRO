df2eb1ba890f2fcd9b834c4d4d96dd7796029d94
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-24 13:44:04 +0200

    Add scheduled task endpoint

--- api/tareaProgramada.js
+++ api/tareaProgramada.js
@@ -1,32 +1,12 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+// api/tareaProgramada.js
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Método no permitido' });
-    return;
-  }
-
-  try {
-    if (!client.topology || !client.topology.isConnected()) {
-      await client.connect();
+module.exports = (req, res) => {
+    if (req.method === 'POST') {
+      // Lógica para manejar la tarea programada
+      console.log("Tarea programada ejecutada.");
+      res.status(200).json({ message: "Tarea programada ejecutada correctamente." });
+    } else {
+      res.status(405).json({ message: "Método no permitido." });
     }
-
-    const db = client.db('abmUsers');
-    const collection = db.collection('users');
-
-    const result = await collection.updateMany(
-      { 'calories.value': { $exists: true } },
-      { $set: { 'calories.$[].value': 0 } }
-    );
-
-    console.log('Número de documentos actualizados:', result.modifiedCount);
-    res.status(200).json({ message: 'Tarea programada ejecutada correctamente' });
-  } catch (error) {
-    console.error('Error ejecutando la tarea programada:', error);
-    res.status(500).json({ message: 'Error ejecutando la tarea programada' });
-  }
-};
+  };
+  
