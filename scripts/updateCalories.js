980646d6b5771364679722cbbd874a2f2aaf6b57
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-26 10:42:38 +0200

    prueba

--- scripts/updateCalories.js
+++ scripts/updateCalories.js
@@ -1,38 +1,34 @@
+// scripts/updateCalories.js
 const { MongoClient } = require('mongodb');
 
-module.exports = async (req, res) => {
+// Obtén la URI de la base de datos y el secreto del entorno
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.error('Error: MONGODB_URI no está definido.');
+  process.exit(1);
+}
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function updateCalories() {
   console.log('Inicio de la ejecución del script.');
 
-  const authHeader = req.headers.authorization;
-  const expectedAuthHeader = `Bearer ${process.env.CRON_SECRET}`;
-
-  // Verificar el secreto de autenticación
-  if (!authHeader || authHeader !== expectedAuthHeader) {
-    console.log('Autenticación fallida.');
-    return res.status(401).json({ success: false, message: 'Unauthorized' });
-  }
-
-  // Conectar a MongoDB
-  const uri = process.env.MONGODB_URI;
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
   try {
-    console.log('Conectando a MongoDB...');
     await client.connect();
     const db = client.db('abmUsers');
     const collection = db.collection('users');
 
-    // Actualizar los documentos
-    console.log('Actualizando documentos...');
+    // Actualizar los documentos de calorías
     const result = await collection.updateMany({}, { $set: { 'calories.$[].value': 0 } });
-    console.log(`Número de documentos actualizados: ${result.modifiedCount}`);
-    
-    res.status(200).json({ success: true, message: `${result.modifiedCount} documents updated` });
+    console.log(`${result.modifiedCount} documentos actualizados.`);
   } catch (error) {
-    console.error('Error en la actualización de calorías:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    console.error('Error al actualizar las calorías:', error);
   } finally {
     await client.close();
     console.log('Conexión a MongoDB cerrada.');
   }
-};
+}
+
+// Ejecutar la función
+updateCalories();
+
