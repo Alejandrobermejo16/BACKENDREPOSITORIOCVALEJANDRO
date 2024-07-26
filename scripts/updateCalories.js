682a67c9c7f17dce8dd67c220da311626c5f9022
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-26 10:25:29 +0200

    cambio version node

--- scripts/updateCalories.js
+++ scripts/updateCalories.js
@@ -1,28 +1,38 @@
-// scripts/updateCalories.js
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+module.exports = async (req, res) => {
+  console.log('Inicio de la ejecución del script.');
 
-async function updateCalories() {
+  const authHeader = req.headers.authorization;
+  const expectedAuthHeader = `Bearer ${process.env.CRON_SECRET}`;
+
+  // Verificar el secreto de autenticación
+  if (!authHeader || authHeader !== expectedAuthHeader) {
+    console.log('Autenticación fallida.');
+    return res.status(401).json({ success: false, message: 'Unauthorized' });
+  }
+
+  // Conectar a MongoDB
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
   try {
+    console.log('Conectando a MongoDB...');
     await client.connect();
     const db = client.db('abmUsers');
     const collection = db.collection('users');
 
-    // Actualizar el valor de las calorías a 0
-    const result = await collection.updateMany(
-      { 'calories.value': { $exists: true } },
-      { $set: { 'calories.$[].value': 0 } }
-    );
-
+    // Actualizar los documentos
+    console.log('Actualizando documentos...');
+    const result = await collection.updateMany({}, { $set: { 'calories.$[].value': 0 } });
     console.log(`Número de documentos actualizados: ${result.modifiedCount}`);
+    
+    res.status(200).json({ success: true, message: `${result.modifiedCount} documents updated` });
   } catch (error) {
-    console.error('Error al actualizar las calorías:', error);
+    console.error('Error en la actualización de calorías:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
   } finally {
     await client.close();
+    console.log('Conexión a MongoDB cerrada.');
   }
-}
-
-updateCalories();
+};
