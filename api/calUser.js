c2f0bc42e51ceed7f27355c39a9a9a4040fdbcbf
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-08-14 12:56:31 +0200

    prueba

--- api/calUser.js
+++ api/calUser.js
@@ -3,13 +3,34 @@ const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const cron = require('node-cron');
 const router = express.Router();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Mapa de traducción de meses
+const monthTranslations = {
+  'January': 'Enero',
+  'February': 'Febrero',
+  'March': 'Marzo',
+  'April': 'Abril',
+  'May': 'Mayo',
+  'June': 'Junio',
+  'July': 'Julio',
+  'August': 'Agosto',
+  'September': 'Septiembre',
+  'October': 'Octubre',
+  'November': 'Noviembre',
+  'December': 'Diciembre'
+};
+
+function translateMonthToSpanish(month) {
+  return monthTranslations[month] || month;  // Usa el mismo nombre si no hay traducción
+}
+
 // Configuración de CORS
 router.use(cors());
 router.use(bodyParser.json());
+
 // Middleware para conectar a la base de datos
 router.use(async (req, res, next) => {
   try {
@@ -21,14 +42,15 @@ router.use(async (req, res, next) => {
     next();
   } catch (error) {
     console.error('Error al conectar con MongoDB:', error);
-    res.status(500).json({ message: 'Error connecting to database' });
+    res.status(500).json({ message: 'Error al conectar con la base de datos' });
   }
 });
+
 // Verificar si el usuario tiene registros de calorías
 router.get('/cal', async (req, res) => {
   const { userEmail } = req.query;
   if (!userEmail) {
-    return res.status(400).json({ message: 'Email is required' });
+    return res.status(400).json({ message: 'Se requiere el correo electrónico' });
   }
   try {
     const db = req.dbClient.db('abmUsers');
@@ -39,17 +61,18 @@ router.get('/cal', async (req, res) => {
     if (user && user.calories && user.calories.length > 0) {
       return res.status(200).json({ calories: user.calories });
     }
-    res.status(404).json({ message: 'No calories record found for this user' });
+    res.status(404).json({ message: 'No se encontraron registros de calorías para este usuario' });
   } catch (error) {
-    console.error('Error retrieving calories:', error);
-    res.status(500).json({ message: 'Error retrieving calories' });
+    console.error('Error al recuperar las calorías:', error);
+    res.status(500).json({ message: 'Error al recuperar las calorías' });
   }
 });
+
 // Actualizar calorías (PUT)
 router.put('/cal', async (req, res) => {
   const { userEmail, calories, CalMonth } = req.body;
   if (!userEmail || calories == null || !CalMonth) {
-    return res.status(400).json({ message: 'Email, calories, and CalMonth are required' });
+    return res.status(400).json({ message: 'Correo electrónico, calorías y CalMonth son requeridos' });
   }
 
   try {
@@ -61,19 +84,22 @@ router.put('/cal', async (req, res) => {
     const [currentDay] = Object.keys(CalMonth[currentMonth].days);
     const updatedCalories = CalMonth[currentMonth].days[currentDay].calories;
 
+    // Traducir el mes al español
+    const translatedMonth = translateMonthToSpanish(currentMonth);
+
     // Buscar el usuario
     const user = await collection.findOne({ email: userEmail });
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: 'Usuario no encontrado' });
     }
 
     // Verificar si el mes y el día existen en el documento del usuario
-    const monthExists = user.CalMonth && user.CalMonth[currentMonth];
-    const dayExists = monthExists && user.CalMonth[currentMonth].days && user.CalMonth[currentMonth].days[currentDay];
+    const monthExists = user.CalMonth && user.CalMonth[translatedMonth];
+    const dayExists = monthExists && user.CalMonth[translatedMonth].days && user.CalMonth[translatedMonth].days[currentDay];
 
     if (!monthExists || !dayExists) {
-      return res.status(404).json({ message: 'Month or day not found' });
+      return res.status(404).json({ message: 'Mes o día no encontrado' });
     }
 
     // Actualizar las calorías solo si hay cambios
@@ -81,7 +107,7 @@ router.put('/cal', async (req, res) => {
       { email: userEmail },
       {
         $set: {
-          [`CalMonth.${currentMonth}.days.${currentDay}.calories`]: updatedCalories,
+          [`CalMonth.${translatedMonth}.days.${currentDay}.calories`]: updatedCalories,
           'calories.$[elem].value': calories.value,
           'calories.$[elem].date': new Date(calories.date),
         }
@@ -93,22 +119,21 @@ router.put('/cal', async (req, res) => {
     );
 
     if (updateResult.modifiedCount > 0) {
-      return res.status(200).json({ message: 'Calories updated successfully' });
+      return res.status(200).json({ message: 'Calorías actualizadas correctamente' });
     }
 
-    res.status(404).json({ message: 'No changes detected or record not found' });
+    res.status(404).json({ message: 'No se detectaron cambios o registro no encontrado' });
   } catch (error) {
-    console.error('Error updating calories:', error);
-    res.status(500).json({ message: 'Error updating calories' });
+    console.error('Error al actualizar las calorías:', error);
+    res.status(500).json({ message: 'Error al actualizar las calorías' });
   }
 });
-
 
 // Crear o actualizar calorías (POST)
 router.post('/cal', async (req, res) => {
   const { userEmail, calories, CalMonth } = req.body;
   if (!userEmail || calories == null || !CalMonth) {
-    return res.status(400).json({ message: 'Email, calories, and CalMonth are required' });
+    return res.status(400).json({ message: 'Correo electrónico, calorías y CalMonth son requeridos' });
   }
 
   try {
@@ -120,11 +145,14 @@ router.post('/cal', async (req, res) => {
     const [currentDay] = Object.keys(CalMonth[currentMonth].days);
     const updatedCalories = CalMonth[currentMonth].days[currentDay].calories;
 
+    // Traducir el mes al español
+    const translatedMonth = translateMonthToSpanish(currentMonth);
+
     // Buscar el usuario
     const user = await collection.findOne({ email: userEmail });
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: 'Usuario no encontrado' });
     }
 
     // Actualizar o agregar el mes y el día
@@ -132,25 +160,24 @@ router.post('/cal', async (req, res) => {
       { email: userEmail },
       {
         $set: {
-          // Crear o actualizar el mes con el día y las calorías
-          [`CalMonth.${currentMonth}.days.${currentDay}.calories`]: updatedCalories,
+          [`CalMonth.${translatedMonth}.days.${currentDay}.calories`]: updatedCalories,
         },
         $push: { calories: { value: calories.value, date: new Date(calories.date) } }
       },
       { upsert: true }
     );
 
-    res.status(201).json({ message: 'Calories created successfully', data: result });
+    res.status(201).json({ message: 'Calorías creadas correctamente', data: result });
   } catch (error) {
-    console.error('Error creating calories:', error);
-    res.status(500).json({ message: 'Error creating calories' });
+    console.error('Error al crear las calorías:', error);
+    res.status(500).json({ message: 'Error al crear las calorías' });
   }
 });
-
 
 // Middleware de manejo de errores
 router.use((err, req, res, next) => {
   console.error('Error en Express middleware:', err);
-  res.status(500).json({ message: 'Something broke!' });
+  res.status(500).json({ message: '¡Algo salió mal!' });
 });
+
 module.exports = router;
