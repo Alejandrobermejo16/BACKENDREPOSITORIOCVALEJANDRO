4c8cc0dad6f3f597abea91df0adaddba0cecf719
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-08-16 14:10:16 +0200

    prueba por rollback incorrecto

--- api/calUser.js
+++ api/calUser.js
@@ -185,31 +185,21 @@ router.put('/cal', async (req, res) => {
   if (!userEmail || calories == null || !CalMonth) {
     return res.status(400).json({ message: 'Email, calories, and CalMonth are required' });
   }
-
   try {
     const db = req.dbClient.db('abmUsers');
     const collection = db.collection('users');
-
-    const dayPath = `CalMonth.${mesActualEnEspañol}.days.${dia}`;
-
     // Obtener la fecha actual
     const fechaActual = new Date();
-
     // Formatear mes y día en español
     const mesActualEnEspañol = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(fechaActual);
     const dia = fechaActual.getDate(); // Día actual del mes
-
     // Construir la ruta de actualización dinámica para `CalMonth`
     const updatePath = `CalMonth.${mesActualEnEspañol}.days.${dia}.calories`;
-
     // Verificar si el usuario existe
     const user = await collection.findOne({ email: userEmail });
-
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-
-
 
     // Obtener el índice del último elemento del array `calories`
     const lastIndex = user.calories.length - 1;
@@ -236,15 +226,12 @@ router.put('/cal', async (req, res) => {
         { email: userEmail },
         {
           $set: {
-            [`CalMonth.${mesActualEnEspañol}.days.${dia}`]: { // Crea un objeto en la clave dinámica
-              [dayPath]: { // Crea un objeto en la clave dinámica
-                value: calories.value, // Propiedad dentro del objeto
-                date: new Date(calories.date) // Otra propiedad dentro del objeto
-              }
-            },
             [`calories.${lastIndex}.value`]: calories.value, // Actualiza el valor de calorías en el array
             [`calories.${lastIndex}.date`]: new Date(calories.date), // Actualiza la fecha en el array
-            
+            [`CalMonth.${mesActualEnEspañol}.days.${dia}`]: {
+              calories: calories.value,
+              date: new Date(calories.date) // Establece la fecha del nuevo día
+            }
           }
         }
       );
