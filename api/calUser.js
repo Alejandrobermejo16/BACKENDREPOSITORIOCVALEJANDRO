8aee6211ddbd89960b560b35a133a8dc95b0eebc
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-08-16 20:13:48 +0200

    cambios

--- api/calUser.js
+++ api/calUser.js
@@ -116,56 +116,115 @@ router.get('/cal', async (req, res) => {
 
 
 //PETICION PUT
- router.put('/cal', async (req, res) => {
-   const { userEmail, calories, CalMonth } = req.body;
-   if (!userEmail || calories == null || !CalMonth) {
-     return res.status(400).json({ message: 'Email, calories, and CalMonth are required' });
-   }
-   try {
-     const db = req.dbClient.db('abmUsers');
-     const collection = db.collection('users');
-     // Obtener la fecha actual
-     const fechaActual = new Date();
-     // Formatear mes y día en español
-     const mesActualEnEspañol = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(fechaActual);
-     const dia = fechaActual.getDate(); // Día actual del mes
-     // Construir la ruta de actualización dinámica para `CalMonth`
-     const updatePath = `CalMonth.${mesActualEnEspañol}.days.${dia}.calories`;
-     // Verificar si el usuario existe
-     const user = await collection.findOne({ email: userEmail });
-     if (!user) {
-       return res.status(404).json({ message: 'User not found' });
-     }
-     // Si el array `calories` está vacío, no hay nada que actualizar
-     if (user.calories.length === 0) {
-       return res.status(400).json({ message: 'No calorie entries found to update' });
-     }
-     // Obtener el índice del último elemento del array `calories`
-     const lastIndex = user.calories.length - 1;
-     // Actualizar el último elemento del array `calories`
-     const result = await collection.updateOne(
-       { email: userEmail },
-       {
-         $set: {
-           [`calories.${lastIndex}.value`]: calories.value, // Actualiza el valor de calorías
-           [`calories.${lastIndex}.date`]: new Date(calories.date), // Actualiza la fecha de calorías
-           [updatePath]: calories.value // Actualiza el campo en `CalMonth`
-         }
-       }
-     );
-     if (result.modifiedCount > 0) {
-       return res.status(200).json({ message: 'Calories updated successfully' });
-     } else {
-       return res.status(404).json({ message: 'No calories to update' });
-     }
-   } catch (error) {
-     console.error('Error actualizando las calorías:', error);
-     return res.status(500).json({
-       message: 'Error actualizando las calorías',
-       errorDetails: error.message
-     });
-   }
- });
+//  router.put('/cal', async (req, res) => {
+//    const { userEmail, calories, CalMonth } = req.body;
+//    if (!userEmail || calories == null || !CalMonth) {
+//      return res.status(400).json({ message: 'Email, calories, and CalMonth are required' });
+//    }
+//    try {
+//      const db = req.dbClient.db('abmUsers');
+//      const collection = db.collection('users');
+//      // Obtener la fecha actual
+//      const fechaActual = new Date();
+//      // Formatear mes y día en español
+//      const mesActualEnEspañol = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(fechaActual);
+//      const dia = fechaActual.getDate(); // Día actual del mes
+//      // Construir la ruta de actualización dinámica para `CalMonth`
+//      const updatePath = `CalMonth.${mesActualEnEspañol}.days.${dia}.calories`;
+//      // Verificar si el usuario existe
+//      const user = await collection.findOne({ email: userEmail });
+//      if (!user) {
+//        return res.status(404).json({ message: 'User not found' });
+//      }
+//      // Si el array `calories` está vacío, no hay nada que actualizar
+//      if (user.calories.length === 0) {
+//        return res.status(400).json({ message: 'No calorie entries found to update' });
+//      }
+//      // Obtener el índice del último elemento del array `calories`
+//      const lastIndex = user.calories.length - 1;
+//      // Actualizar el último elemento del array `calories`
+//      const result = await collection.updateOne(
+//        { email: userEmail },
+//        {
+//          $set: {
+//            [`calories.${lastIndex}.value`]: calories.value, // Actualiza el valor de calorías
+//            [`calories.${lastIndex}.date`]: new Date(calories.date), // Actualiza la fecha de calorías
+//            [updatePath]: calories.value // Actualiza el campo en `CalMonth`
+//          }
+//        }
+//      );
+//      if (result.modifiedCount > 0) {
+//        return res.status(200).json({ message: 'Calories updated successfully' });
+//      } else {
+//        return res.status(404).json({ message: 'No calories to update' });
+//      }
+//    } catch (error) {
+//      console.error('Error actualizando las calorías:', error);
+//      return res.status(500).json({
+//        message: 'Error actualizando las calorías',
+//        errorDetails: error.message
+//      });
+//    }
+//  });
+
+router.put('/cal', async (req, res) => {
+  const { userEmail, calories, CalMonth } = req.body;
+  if (!userEmail || calories == null || !CalMonth) {
+    return res.status(400).json({ message: 'Email, calories, and CalMonth are required' });
+  }
+  try {
+    const db = req.dbClient.db('abmUsers');
+    const collection = db.collection('users');
+    // Obtener la fecha actual
+    const fechaActual = new Date();
+    // Formatear mes y día en español
+    const mesActualEnEspañol = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(fechaActual);
+    const dia = fechaActual.getDate(); // Día actual del mes
+    // Construir la ruta de actualización dinámica para `CalMonth`
+    const updatePath = `CalMonth.${mesActualEnEspañol}.days.${dia}.calories`;
+    // Verificar si el usuario existe
+    const user = await collection.findOne({ email: userEmail });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    // Si el array `calories` está vacío, no hay nada que actualizar
+    if (user.calories.length === 0) {
+      return res.status(400).json({ message: 'No calorie entries found to update' });
+    }
+    // Obtener el índice del último elemento del array `calories`
+    const lastIndex = user.calories.length - 1;
+    // Actualizar el último elemento del array `calories`
+    const result = await collection.updateOne(
+      { email: userEmail },
+      {
+        
+        $set: {
+          [`calories.${lastIndex}.value`]: calories.value, // Actualiza el valor de calorías
+          [`calories.${lastIndex}.date`]: new Date(calories.date), // Actualiza la fecha de calorías
+          [updatePath]: calories.value // Actualiza el campo en `CalMonth`
+        }
+      },
+      {
+        $push: {
+          [`CalMonth.months.${monthIndex}.days`]: {
+            day: 11, // Día del nuevo registro
+            calories: 123, // Valor de calorías de prueba
+            date: "2024-08-17T00:00:00.000Z" // Fecha de prueba
+          }}}
+    );
+    if (result.modifiedCount > 0) {
+      return res.status(200).json({ message: 'Calories updated successfully' });
+    } else {
+      return res.status(404).json({ message: 'No calories to update' });
+    }
+  } catch (error) {
+    console.error('Error actualizando las calorías:', error);
+    return res.status(500).json({
+      message: 'Error actualizando las calorías',
+      errorDetails: error.message
+    });
+  }
+});
 
 
 
