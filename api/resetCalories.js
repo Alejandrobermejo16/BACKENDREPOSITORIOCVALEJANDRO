7e24fa888dcbbee42890c361b6f618d66dca1281
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-08-26 11:42:43 +0200

    cambio en el endpoint de reseteo de calorias

--- api/resetCalories.js
+++ api/resetCalories.js
@@ -11,19 +11,19 @@ const resetCalories = async () => {
     const db = client.db('abmUsers');
     const collection = db.collection('users');
 
-    // Consulta para encontrar usuarios con registros de calorías
-    const usersWithCalories = await collection.find({ 'calories.0': { $exists: true } }).toArray();
-    console.log(`Usuarios encontrados con registros de calorías: ${usersWithCalories.length}`); // Añadido para depuración
+    // Consulta para encontrar usuarios con calorías mayores a 0
+    const usersWithCalories = await collection.find({ 'calories.value': { $gt: 0 } }).toArray();
+    console.log(`Usuarios encontrados con calorías mayores a 0: ${usersWithCalories.length}`); // Añadido para depuración
 
     if (usersWithCalories.length === 0) {
-      console.log('No hay usuarios con registros de calorías.');
-      return 0; // Retorna 0 si no hay usuarios con calorías
+      console.log('No hay usuarios con calorías mayores a 0.');
+      return 0; // Retorna 0 si no hay usuarios con calorías mayores a 0
     }
 
-    // Actualiza las calorías de los usuarios encontrados
+    // Actualiza las calorías de los usuarios encontrados a 0
     const result = await collection.updateMany(
-      { 'calories.0': { $exists: true } },
-      { $set: { 'calories.$[].value': 0 } }
+      { 'calories.value': { $gt: 0 } },
+      { $set: { 'calories.value': 0 } }
     );
     console.log(`Calorías actualizadas para ${result.modifiedCount} usuarios.`); // Añadido para depuración
 
