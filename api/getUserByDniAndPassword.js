f5fa32db8df53f2427800dda38b24ab9d97d4c76
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-10-04 21:02:19 +0200

    quitar devolucion de datos de usuario

--- api/getUserByDniAndPassword.js
+++ api/getUserByDniAndPassword.js
@@ -63,7 +63,7 @@ router.post('/getUserByDniAndPassword', async (req, res) => {
         return res.status(401).json({ message: 'Invalid credentials' });
       }
   
-      res.status(200).json(user); // Retornar el usuario encontrado
+      res.status(200); 
     } catch (error) {
       console.error('Error al obtener el usuario:', error);
       res.status(500).json({ message: 'Error fetching user' });
