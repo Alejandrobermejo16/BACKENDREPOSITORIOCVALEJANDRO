5ec643d976482c2661aee7144a6d57262d3f2dd7
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-10-04 21:05:37 +0200

    devolucion de mensaje correcto

--- api/getUserByDniAndPassword.js
+++ api/getUserByDniAndPassword.js
@@ -40,35 +40,35 @@ router.use(async (req, res, next) => {
 
 // Ruta POST para obtener un usuario por DNI y contraseña
 router.post('/getUserByDniAndPassword', async (req, res) => {
-    const dbClient = req.dbClient;
-    const { dni, password } = req.body; // Obtener DNI y contraseña del cuerpo de la solicitud
-  
-    try {
-      const database = dbClient.db('abmUsers');
-      const collection = database.collection('usersBank');
-  
-      // Buscar el usuario con el DNI 
-      const user = await collection.findOne({ dni: dni });
-  
-      // Verificar si se encontró el usuario
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Verifica en formato hash si la contraseña introducida y convertida a hash es igual que la que hay en la base de datos
-      const isMatch = await bcrypt.compare(password, user.pass);
-  
-      // Verificar si la contraseña es válida
-      if (!isMatch) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
-  
-      res.status(200); 
-    } catch (error) {
-      console.error('Error al obtener el usuario:', error);
-      res.status(500).json({ message: 'Error fetching user' });
+  const dbClient = req.dbClient;
+  const { dni, password } = req.body; // Obtener DNI y contraseña del cuerpo de la solicitud
+
+  try {
+    const database = dbClient.db('abmUsers');
+    const collection = database.collection('usersBank');
+
+    // Buscar el usuario con el DNI 
+    const user = await collection.findOne({ dni: dni });
+
+    // Verificar si se encontró el usuario
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
-  });
-  
+
+    // Verifica en formato hash si la contraseña introducida y convertida a hash es igual que la que hay en la base de datos
+    const isMatch = await bcrypt.compare(password, user.pass);
+
+    // Verificar si la contraseña es válida
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ message: 'Acceso correcto' }); // Retornar el usuario encontrado
+  } catch (error) {
+    console.error('Error al obtener el usuario:', error);
+    res.status(500).json({ message: 'Error fetching user' });
+  }
+});
+
 
 module.exports = router;
