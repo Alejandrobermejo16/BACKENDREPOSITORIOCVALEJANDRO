db13d811d2fa680053be949c5718cd4d4d14f391
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-12-09 10:39:49 +0100

    changes of the get endpoint

--- api/getSections.js
+++ api/getSections.js
@@ -21,4 +21,25 @@ router.get('/getSections', async (req, res) => {
     }
 });
 
+router.get('/getContent', async (req, res) => {
+    const dbClient = req.dbClient;
+
+    try {
+        const database = dbClient.db('abmUsers');
+        const collection = database.collection('abmSections');
+        
+        const sections = await collection.find({}).toArray();
+
+        if (sections.length === 0) {
+            return res.status(404).json({ message: 'No secciones encontradas' });
+        }
+
+       
+    } catch (error) {
+        console.error('Error al obtener las secciones:', error);
+        res.status(500).json({ message: 'Error al obtener las secciones' });
+    }
+});
+
+
 module.exports = router;
